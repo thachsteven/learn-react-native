63f6bff69d793d76d0e401a5a835c54d58bfc73c
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-19 22:54:39 +0700

    Project init

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Pipeline failing? To keep GitLab spam and abuse free we ask that you
+        verify your identity. Pipeline failing? To keep GitLab spam and abuse
+        free we ask that you verify your identity. Pipeline failing? To keep
+        GitLab spam and abuse free we ask that you verify your identity.
+        Pipeline failing? To keep GitLab spam and abuse free we ask that you
+        verify your identity.
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
