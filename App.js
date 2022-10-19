d318447a166ad299d8b9c6414e72cf1e0c2e71b5
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-19 23:13:00 +0700

    Button

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { Button, SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hi</Text>
-      <Image source={require("./assets/favicon.png")} />
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <View style={{ width: 400, height: 400, backgroundColor: "green" }}>
-          <Image
-            fadeDuration={1000}
-            source={{
-              width: 200,
-              height: 300,
-              uri: "https://i.ytimg.com/vi/XekAF8bX51U/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAUVtOBWHOKWRqWvMkWLHi-rAhfYA",
-            }}
-          />
-        </View>
-      </TouchableNativeFeedback>
-
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => console.log("Pressed")}
+      />
     </SafeAreaView>
   );
 }
