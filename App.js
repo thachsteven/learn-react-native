cc3b358d6fd86c12cdd59d71abb3b5071788c9d3
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-19 23:11:10 +0700

    Touchables

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hi</Text>
       <Image source={require("./assets/favicon.png")} />
-      <Image
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://i.ytimg.com/vi/XekAF8bX51U/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAUVtOBWHOKWRqWvMkWLHi-rAhfYA",
-        }}
-      />
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+        <View style={{ width: 400, height: 400, backgroundColor: "green" }}>
+          <Image
+            fadeDuration={1000}
+            source={{
+              width: 200,
+              height: 300,
+              uri: "https://i.ytimg.com/vi/XekAF8bX51U/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAUVtOBWHOKWRqWvMkWLHi-rAhfYA",
+            }}
+          />
+        </View>
+      </TouchableNativeFeedback>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
