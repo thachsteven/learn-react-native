17ce62a2e98f6e4b4c29d9aa461a7f748dbe7611
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-19 23:04:21 +0700

    Image

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Text pressed");
-  };
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Pipeline failing? To keep GitLab spam and abuse free we ask that you
-        verify your identity. Pipeline failing? To keep GitLab spam and abuse
-        free we ask that you verify your identity. Pipeline failing? To keep
-        GitLab spam and abuse free we ask that you verify your identity.
-        Pipeline failing? To keep GitLab spam and abuse free we ask that you
-        verify your identity.
-      </Text>
+      <Text>Hi</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Image
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://i.ytimg.com/vi/XekAF8bX51U/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAUVtOBWHOKWRqWvMkWLHi-rAhfYA",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
