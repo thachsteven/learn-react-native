586cf8ec855c53a1a591cffd5d898bf538fb2809
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-20 11:36:30 +0700

    Platform-specific-code

---
+++ app/components/AppText.android.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text } from "react-native";
+
+const AppText = ({ children }) => {
+  return <Text style={styles.text}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: "tomato",
+    fontSize: 20,
+    fontFamily: "Roboto",
+  },
+});
+
+export default AppText;
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -7,8 +7,17 @@ const AppText = ({ children }) => {
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Anenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
   },
 });
 
