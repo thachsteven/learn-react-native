57dacdccd1c48cbe80a1ff6cf2797e64a92cacc2
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-20 11:27:52 +0700

    Icons

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import AppText from "./app/components/AppText";
+import { AntDesign } from "@expo/vector-icons";
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <AppText>hello</AppText>
+      <AntDesign name="mail" size={100} color="red" />
     </View>
   );
 };
