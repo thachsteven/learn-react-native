4b35057d78d89441616702456f8d9933a7f83ee2
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-20 09:16:21 +0700

    Platform-specific-code

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
-import { Alert, Button, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Alert,
+  Button,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        color="orange"
-        title="Click Me"
-        onPress={
-          () =>
-            Alert.alert("Title here", "Message here", [
-              {
-                text: "Yes",
-                onPress: () => console.log("Yes"),
-              },
-              {
-                text: "No",
-                onPress: () => console.log("No"),
-              },
-            ])
-          // Alert.prompt("My Title", "My message", (text) => console.log(text)) //Only IOS works
-        }
-      />
+      <Button color="orange" title="Click Me" />
     </SafeAreaView>
   );
 }
@@ -29,7 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
