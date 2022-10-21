92309b0f8b9d7d2e99766e9b10fe9b69bbd188d7
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-21 10:06:43 +0700

    Building-the-icon-component

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 const App = () => {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 };
 
 export default App;
