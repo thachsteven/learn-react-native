4085c594752838ed3519e0cd51c79d3aca56d052
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-10-21 11:01:01 +0700

    Textinput

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import { useState } from "react";
+import { Text, TextInput } from "react-native";
+import Screen from "./app/components/Screen";
 
 const App = () => {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry
+        clearButtonMode="always" //Only works on IOS
+        keyboardType="numeric"
+        maxLength={5}
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
+  );
 };
 
 export default App;
